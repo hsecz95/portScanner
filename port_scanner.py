1ae9f4bdb64bb299f208e92342de7d07443837c9
Hicham <hicham.ezzamzami@gmail.com>
2021-05-30 00:43:00 +0100

    first update

---
+++ port_scanner.py
@@ -0,0 +1,28 @@
+import socket
+import termcolor
+
+def scan(targets, ports):
+    for port in range(1,ports):
+        scan_port(targets, port)
+
+def scan_port(ipaddress, port):
+    try:
+        sock = socket.socket()
+        sock.connect((ipaddress, port))
+        print ("[+] port open: " + str(port))
+        sock.close()
+    except:
+        #print ("[-] port closed: " + str(port))
+        pass
+
+
+targets = input("[*] Enter targets to scan (split them with ,): ")
+ports = int(input("[*] How many port you want to scan: "))
+
+if "," in targets:
+    print("[*] Scanning Multiple Targets")
+    for ip_addr in targets.split(','):
+        scan(ip_addr.strip(' '), ports)
+else:
+    scan(targets,ports)
+
